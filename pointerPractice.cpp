c272600ddbc563e5432d47f18be631dbdc68e18e
inalazeva09 <inalazeva@yahoo.com>
2026-04-07 10:43:43 -0400

    Add files via upload

---
+++ pointerPractice.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+// void addStuff(int* y) {
+//     *y += 3; 
+// }
+
+// int main(){
+//     int x = 5; 
+//     int* ptr = &x;
+//     cout << x << endl; 
+//     cout << &x << endl; 
+//     cout << ptr << endl;
+//     cout << *ptr << endl;
+
+//     *ptr = 7; 
+//     cout << *ptr << endl; 
+//     cout << "x = " << x << endl; 
+//     addStuff(&x);
+//     cout << "new x = " << x << endl; 
+
+// _________________________________________________________
+
+// int main() {
+//     int num = 10;
+//     int* ptr = &num;
+
+//     cout << "num = " << num << endl;
+//     cout << "Address of num = " << &num << endl;
+//     cout << "ptr = " << ptr << endl;
+//     cout << "Value pointed to by ptr = " << *ptr << endl;
+
+//     return 0;
+// }
+
+// _________________________________________________________
+
+int main() {
+    int num = 50; 
+    int* ptr = &num; 
+
+
+    cout << "Originial value: " << num << endl; 
+}
